8b46890bad1601481b7db3ff7143657e4a985dd4
junyoung93.enm <junyoung93@cj.net>
2022-05-24 21:20:03 +0900

    테스트

---
+++ s.js
@@ -0,0 +1,35 @@
+/**
+ * 숫자 자리수 구하기
+ * @param {} number 
+ * @returns 
+ */
+function getNumberSize(number){
+	let count =1;
+	while(number >= 10){
+		number = number/10
+		count++;
+	}
+	return count;
+}
+
+getNumberSize(12345678);
+
+
+
+function getNumberByIndex(number,index){
+	let x = 1;
+
+	for(let i=1; i<index; i++){
+		x = x*10;
+	}
+	
+	return parseInt(number/x)%10;
+}
+
+
+getNumberByIndex(12345678,1)
+
+
+function getNumberByIndex2(number,index){
+	
+}
