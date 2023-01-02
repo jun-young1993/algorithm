78828c16cd48a49c1f97bb549196d2da68e7034e
kim-jun-young <jykim@gemiso.com>
2023-01-02 14:23:04 +0900

    test

--- s.js
+++ s.js
@@ -1,12 +1,12 @@
-/**
+/** 
  * 숫자 자리수 구하기
  * @param {} number 
  * @returns 
  */
-function getNumberSize(number){
-	let count =1;
-	while(number >= 10){
-		number = number/10
+function getNumberSize(number) {
+	let count = 1;
+	while (number >= 10) {
+		number = number / 10
 		count++;
 	}
 	return count;
@@ -16,20 +16,20 @@ getNumberSize(12345678);
 
 
 
-function getNumberByIndex(number,index){
+function getNumberByIndex(number, index) {
 	let x = 1;
 
-	for(let i=1; i<index; i++){
-		x = x*10;
+	for (let i = 1; i < index; i++) {
+		x = x * 10;
 	}
-	
-	return parseInt(number/x)%10;
+
+	return parseInt(number / x) % 10;
 }
 
 
-getNumberByIndex(12345678,1)
+getNumberByIndex(12345678, 1)
 
 
-function getNumberByIndex2(number,index){
-	
+function getNumberByIndex2(number, index) {
+
 }
